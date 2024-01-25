1fcae10beb555f51128ab1e75a8f16e9d049a4c9
Lucas <=>
2024-01-25 12:34:42 -0300

    atualização Lógica

--- src/components/Inputs/Inputs.tsx
+++ src/components/Inputs/Inputs.tsx
@@ -1,34 +1,42 @@
-import styles from './Inputs.module.css';
+import { InputUser } from "../../Context/InputUserContext";
+import styles from "./Inputs.module.css";
 
 export function Inputs() {
-    return (
-        
-        <section>
-          <div className={styles.inputs}>
-            
-            <label>Contratante</label>
-            <input type="text" id='input1'/>
-            
-            
-            <label>CNPJ</label>
-            <input type="text" id='input2'/>
-            
-            
-            <label>Lorem</label>
-            <input type="text" id='input3'/>
-            
-            
-            <label>Lorem</label>
-            <input type="text" id='input4'/>
+  const { setUserInfo, setCnpjUser } = InputUser();
 
-            <label>Lorem</label>
-            <input type="text" id='input5'/>
+  return (
+    <section>
+      <div className={styles.inputs}>
+        <label>Contratante</label>
+        <input
+          type="text"
+          id="input1"
+          onChange={(e) => {
+            setUserInfo(e.target.value);
+          }}
+        />
 
-            <label>Lorem</label>
-            <input type="text" id='input6'/>
-            
-          </div>
-        </section>
-        
-    )
+        <label>CNPJ</label>
+        <input
+          type="text"
+          id="input2"
+          onChange={(e) => {
+            setCnpjUser(e.target.value);
+          }}
+        />
+
+        <label>Lorem</label>
+        <input type="text" id="input3" />
+
+        <label>Lorem</label>
+        <input type="text" id="input4" />
+
+        <label>Lorem</label>
+        <input type="text" id="input5" />
+
+        <label>Lorem</label>
+        <input type="text" id="input6" />
+      </div>
+    </section>
+  );
 }
