5a331cc77e98a01dd185cd48253ede55a4a281f9
Lucas Rodrigues Seidel Silva <lucas.rodrigues28012003@gmail.com>
2024-02-28 15:19:35 -0300

    ajuste rapido

--- src/components/Visualization/Visualization.tsx
+++ src/components/Visualization/Visualization.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import styles from "./Visualization.module.css";
 import html2pdf from "html2pdf.js";
 import { Switcher } from "../Switcher/Switcher";
@@ -7,22 +8,21 @@ export function handleUserClick() {
 
   html2pdf(contentElement, {
     margin: 8.5,
-  filename: "ContratoInfra.pdf",
-  image: { type: "jpeg", quality: 0.98 },
-  html2canvas: { scale: 2 },
-  jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-  pagebreak: { mode: ['css']}
-  }).then((pdf) => {
+    filename: "ContratoInfra.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    pagebreak: { mode: ["css"] },
+  }).then((pdf: { save: (arg0: string) => void }) => {
     pdf.save("ContratoInfra.pdf");
   });
 }
 
 export function Visualization() {
-
   return (
     <div>
       <section className={styles.visualization}>
-        <Switcher/>
+        <Switcher />
       </section>
     </div>
   );
